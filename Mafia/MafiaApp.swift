c83cb8ef9acd9ddbf1350cad168238bbd43b7718
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2023-07-27 12:14:20 +0400

    Initial Commit

---
+++ Mafia/MafiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MafiaApp.swift
+//  Mafia
+//
+//  Created by Eduard Bagdagulyan on 27.07.23.
+//
+
+import SwiftUI
+
+@main
+struct MafiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
